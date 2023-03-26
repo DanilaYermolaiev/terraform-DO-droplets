cc995aac755fb14692e3de61b2f81b9a7fcc2c2f
case0sh <cubikline@gmail.com>
2023-03-26 16:09:23 +0200

    udpdate second repo and cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ data "aws_ami" "al2_latest" {
 }
 
 resource "aws_instance" "webserver" {
-  ami                         = data.aws_ami.ubuntu-linux-1804.id
+  ami                         = data.aws_ami.ubuntu-linux-2004.id
   instance_type               = var.instance_type
   user_data                   = file("aws-user-data.sh")
   key_name                    = aws_key_pair.ansible_keypair.key_name
@@ -66,6 +66,18 @@ data "aws_ami" "ubuntu-linux-1804" {
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_ami" "ubuntu-linux-2004" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
