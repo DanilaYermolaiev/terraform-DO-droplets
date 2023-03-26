97ef3168d61dcf9790ea3b41ad39a4a6ccdc884d
case0sh <cubikline@gmail.com>
2023-03-26 21:09:04 +0200

    udpdate second repo and cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "webserver_sg" {
 
 # EC2 instance
 
-}
+
 resource "aws_instance" "webserver" {
   ami                         = data.aws_ami.ubuntu-linux-2004.id
   instance_type               = var.instance_type
