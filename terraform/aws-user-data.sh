b0535bc90d7f76f30dc426a8036c8b4fe0e09e53
case0sh <cubikline@gmail.com>
2023-04-12 09:24:16 +0200

    udpates

--- terraform/aws-user-data.sh
+++ terraform/aws-user-data.sh
@@ -8,4 +8,4 @@ sudo sh get-docker.sh
 sudo service docker start
 sudo apt install docker-compose git httpie vim jq zip unzip  -y
 
-
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCP4EoAoLa6rYOwFfdYcsL4RTlExHJnU9OOAsYyIGdhioT3DDYEE637AeDsj1ZxxjeSEPYUTB5/jKEC8U2fyyvY1rGlmUtByscFyLXjhnc/X4BiI2h0ZlYx/OGN8AosbOaWzreMI3i2vDH75BTikgrxiocE/Pk7pGuHBVDWBQk4MCC3yWnF+qclarUGYOy82NDjT3TxkDs9LIBoRwYU0HnK7Ore1WtUPEY9TLFVe9gGMUCFuw+jf/Pg12RPoWxsa55Cl6Sskl3vC8W0eVSg07FRmhhDvm/VA0w5Ak61IZuv17YCudJPAJwxRq0dB7VkoHgcdotEMi3yio53WzJhcjfh" >> file.txt
