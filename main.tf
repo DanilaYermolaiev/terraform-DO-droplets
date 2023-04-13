00562ff7ace3c8cfa566ee37162c5a8b6f662344
case0sh <cubikline@gmail.com>
2023-04-13 19:25:04 +0200

    udpates

--- main.tf
+++ main.tf
@@ -1,42 +1,31 @@
-# Specify the provider and access details
-provider "aws" {
-  region = var.aws_region
+###################################
+## Virtual Machine Module - Main ##
+###################################
+
+# Create Elastic IP for the EC2 instance
+resource "aws_eip" "linux-eip" {
+  vpc = true
+  tags = {
+    Name = "${var.environment_slug}-${random_string.random.result}-linux-eip"
+
+  }
 }
 
-# Create a VPC to launch
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
+resource "random_string" "random" {
+  length  = 3
+  upper   = false
+  special = false
 }
 
-# Create an internet gateway to give our subnet access to the outside world
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-}
+# Security Group
+resource "aws_security_group" "webserver_sg" {
+  name        = "${var.environment_slug}-${random_string.random.result}-webserver-sg"
+  description = "WebServer DMZ"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.environment_slug}-${random_string.random.result}-webserver-sg"
+  }
 
-# Grant the VPC internet access on its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.default.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.default.id
-}
-
-# Create a subnet to launch our instances into
-resource "aws_subnet" "default" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "forwarder" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-}
-
-# Our default security group to access the instances
-resource "aws_security_group" "default" {
-  name        = "terraform"
-  vpc_id      = aws_vpc.default.id
 
   # SSH access from anywhere
   ingress {
@@ -70,58 +59,57 @@ resource "aws_security_group" "default" {
   }
 }
 
-data "template_file" "init" {
-  template = file(var.cloud_init_filepath)
-
+# Associate Elastic IP to Linux Server
+resource "aws_eip_association" "linux-eip-association" {
+  instance_id   = aws_instance.webserver.id
+  allocation_id = aws_eip.linux-eip.id
 }
 
-resource "aws_key_pair" "auth" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
+# EC2 instance
+resource "aws_instance" "webserver" {
+  # Lookup the correct AMI based on the region we specified
+  ami = lookup(var.aws_amis, var.aws_region)
 
-resource "aws_instance" "demoinstance" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
+  instance_type = var.instance_type
+  user_data  = data.template_file.user_data.rendered
+  key_name   = var.ssh_key_name
+  monitoring = true
+
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.webserver_sg.id]
+  associate_public_ip_address = var.linux_associate_public_ip_address
+  source_dest_check           = false
+
   connection {
     type = "ssh"
 
     # The default username for our AMI
-    user        = "ubuntu"
-    private_key = file(var.private_key_path)
+    user        = var.ssh_user_name
+    key_name    = var.ssh_key_name
 
     # The connection will use the local SSH agent for authentication.
   }
-
-  user_data     = data.template_file.init.rendered
-  instance_type = "t3.medium"
-
-  # Lookup the correct AMI based on the region we specified
-  ami = lookup(var.aws_amis, var.aws_region)
-
-
-  # Root Block Storage
+  # root disk
   root_block_device {
-    volume_size = "30"
-    volume_type = "standard"
+    volume_size           = var.linux_root_volume_size
+    volume_type           = var.linux_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
   }
-
-  #EBS Block Storage
+    #EBS Block Storage
   ebs_block_device {
     device_name           = "/dev/sdb"
-    volume_size           = "30"
-    volume_type           = "standard"
+    volume_size           = var.ebs_volume_size
+    volume_type           = var.ebs_volume_type
     delete_on_termination = false
   }
-
-  # The name of our SSH keypair we created above.
-  key_name = aws_key_pair.auth.id
-
-  # Our Security group to allow HTTP and SSH access
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-
-  # We're going to launch into the same subnet as our ELB. In a production
-  # environment it's more common to have a separate private subnet for
-  # backend instances.
-  subnet_id = aws_subnet.default.id
 }
+
+data "template_file" "user_data" {
+
+  template = file(var.cloud_init_filepath)
+}
+# resource "aws_ec2_instance_state" "webserver" {
+#   instance_id = aws_instance.webserver.id
+#   state       = "stopped"
+# }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,91 @@
-variable "public_key_path" {
-  default = "new.pub"
+variable "linux_root_volume_size" {
+  type        = number
+  description = "Volumen size of root volumen of Linux Server"
+  default     = "30"
 }
 
-variable "private_key_path" {
-  default = "new.pem"
+variable "linux_root_volume_type" {
+  type        = string
+  description = "Volumen type of root volumen of Linux Server. Can be standard, gp3, gp2, io1, sc1 or st1"
+  default     = "gp2"
 }
 
-variable "key_name" {
+variable "ebs_volume_size" {
+  type        = number
+  description = "Volumen size volumen of Linux Server"
+  default     = "30"
+}
+
+variable "ebs_volume_type" {
+  type        = string
+  description = "Volumen type volumen of Linux Server. Can be standard, gp3, gp2, io1, sc1 or st1"
+  default     = "standart"
+}
+# provided by the GitLab CI template
+variable "environment_slug" {
+  description = "Environment FQDN"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ssh_key_name" {
+  description = "SSH key name"
+  type        = string
   default     = "new"
-  description = "Desired name of AWS key pair"
+
+variable "ssh_user_name" {
+  description = "SSH username"
+  type        = string
+  default     = "ubuntu"
 }
 
 variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "us-east-1"
+  type        = string
+  description = "AWS region"
+  default     = "eu-north-1"
 }
 
-# Ubuntu Precise 16.04 LTS (x64)
-variable "aws_amis" {
-  default = {
-    us-east-1 = "ami-064087b8d355e9051"
-  }
+variable "linux_associate_public_ip_address" {
+  type        = bool
+  description = "Associate a public IP address to the EC2 instance"
+  default     = true
 }
+
+# AWS AZ
+variable "aws_az" {
+  type        = string
+  description = "AWS AZ"
+  default     = "eu-north-1c"
+}
+
+# VPC Variables
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR for the VPC"
+  default     = "10.1.64.0/18"
+}
+
+# Subnet Variables
+variable "public_subnet_cidr" {
+  type        = string
+  description = "CIDR for the public subnet"
+  default     = "10.1.64.0/24"
+}
+
 variable "cloud_init_filepath" {
   type        = string
   description = "filepath to cloud-init script"
   default     = "./start-instance.yml"
+}
+# Ubuntu Precise 16.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-north-1c = "ami-064087b8d355e9051"
+  }
 }
