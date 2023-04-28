f494165f536b14b9b9cccb7c59f23f22bb2a529e
case0sh <cubikline@gmail.com>
2023-04-28 16:57:25 +0200

    glogal do updates

--- privider.tf
+++ privider.tf
@@ -1,11 +1,17 @@
+# Define required providers
 terraform {
+  # required_version = ">= 1.4.2"
+  # using GitLab http backend
+  # see: https://docs.gitlab.com/ee/user/infrastructure/terraform_state.html
+  backend "http" {
+    # auto-configured by the template
+  }
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
     }
   }
 }
-
 provider "digitalocean" {
   token = var.do_token
 }
@@ -13,4 +19,3 @@ provider "digitalocean" {
 data "digitalocean_ssh_key" "ssh" {
   name = var.ssh_name
 }
-
--- variables.tf
+++ variables.tf
@@ -6,30 +6,37 @@ variable "do_token" {
 variable "ssh_name" {
   description   = "Ssh keys in DigitalOcean"
     type        = string
+    default     = "case" # Keys on Digital Ocean
 }
 variable "droplet_image" {
   description   = "Image identifier of the OS in DigitalOcean"
     type        = string
+    default = "ubuntu-22-04-x64" # OS image 
 }
 variable "droplet_region" {
   description = "Droplet region identifier where the droplet will be created"
     type        = string
+    default = "fra1" # Region
 }
 variable "droplet_size" {
   description = "Droplet size identifier"
     type        = string
+    default = "fra1" # Region = "s-1vcpu-2gb" # Server size
 }
 variable "user" {
   description = "Username"
     type        = string
+    default   = "root"
 }
 variable "privatekeypath" {
   description = "Privat ssh key"
     type        = string
+    default = "~/.ssh/id_ed25519" # your privat ssh key - change it
 }
 
 variable "publicekeypath" {
   description = "Public ssh key"
     type        = string
+    default = "~/.ssh/id_ed25519.pub" # your public ssh key - change it
 }
 
